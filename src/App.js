34b4a2616c3ae0582d18ef1a5bd8e853e36ae257
patiltan4 <patiltan4@gmail.com>
2021-09-30 22:20:55 +0530

    added dropdown and fetched list of countries with api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,59 @@
-import React from 'react';
-import './App.css';
+import { FormControl, Select, MenuItem } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+//https://disease.sh/v3/covid-19/countries
 
 function App() {
-  return (
-    <div className="App">
-      <h1>COVID 19 TRACKER</h1>
-    </div>
-  );
+    const [countries, setCountries] = useState(["UK", "USA", "INDIA"]);
+
+    useEffect(() => {
+        console.log("effect");
+        //async
+        const getCountriesData = async () => {
+            await fetch("https://disease.sh/v3/covid-19/countries")
+                .then((resposne) => resposne.json())
+                .then((data) => {
+                    const countries = data.map((country) => ({
+                        name: country.country,
+                        value: country.countryInfo.iso2,
+                    }));
+                    setCountries(countries);
+                });
+        };
+        getCountriesData();
+    }, [countries]);
+
+    return (
+        <div className="app">
+            <div className="app__header">
+                <h1>COVID 19 TRACKER</h1>
+                <FormControl className="app__dropdown">
+                    <Select variant="outlined" value="abc">
+                        {/* Loop through */}
+
+                        {countries.map((country) => (
+                            <MenuItem value={country.value}>
+                                {country.name}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+            </div>
+
+            {/* Header */}
+            {/* Title and select */}
+
+            {/* info  */}
+            {/* info */}
+            {/* info  */}
+
+            {/* Table */}
+            {/* Graph */}
+
+            {/* Map */}
+        </div>
+    );
 }
 
 export default App;
