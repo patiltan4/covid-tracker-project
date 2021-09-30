94e8adcfce8195a834b098ccf7e793b4ccfe7dec
patiltan4 <patiltan4@gmail.com>
2021-09-30 23:30:17 +0530

    left and right part formation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import { FormControl, Select, MenuItem } from "@mui/material";
+import { FormControl, Select, MenuItem, Card, CardContent } from "@mui/material";
 import React, { useState, useEffect } from "react";
 import InfoBox from "./InfoBox";
+import Map from "./Map";
 import "./App.css";
 
 //https://disease.sh/v3/covid-19/countries
@@ -37,42 +38,53 @@ function App() {
 
     return (
         <div className="app">
+          <div className="app__left">
             <div className="app__header">
-                <h1>COVID 19 TRACKER</h1>
-                <FormControl className="app__dropdown">
-                    <Select variant="outlined" onChange={onCountryChange} value={country}>
-                        {/* Loop through */}
-                        <MenuItem value="Worldwide">Worldwide</MenuItem>
+                  <h1>COVID 19 TRACKER</h1>
+                  <FormControl className="app__dropdown">
+                      <Select variant="outlined" onChange={onCountryChange} value={country}>
+                          {/* Loop through */}
+                          <MenuItem value="Worldwide">Worldwide</MenuItem>
 
-                        {countries.map((country) => (
-                            <MenuItem value={country.value}>
-                                {country.name}
-                            </MenuItem>
-                        ))}
-                    </Select>
-                </FormControl>
-            </div>
+                          {countries.map((country) => (
+                              <MenuItem value={country.value}>
+                                  {country.name}
+                              </MenuItem>
+                          ))}
+                      </Select>
+                  </FormControl>
+              </div>
 
-            {/* Header */}
-            {/* Title and select */}
+              {/* Header */}
+              {/* Title and select */}
 
 
-            <div className="app__stats">
-                  <InfoBox title="Covid Cases" cases={12487} total ={2000} />
+              <div className="app__stats">
+                    <InfoBox title="Covid Cases" cases={12487} total ={2000} />
 
-                  <InfoBox title="Recovered" cases={1245} total ={2000}/>
+                    <InfoBox title="Recovered" cases={1245} total ={2000}/>
 
-                  <InfoBox title="Deaths" cases={12499} total ={2000}/>
-            </div>
+                    <InfoBox title="Deaths" cases={12499} total ={2000}/>
+              </div>
 
-            {/* info  */}
-            {/* info */}
-            {/* info  */}
+              {/* info  */}
+              {/* info */}
+              {/* info  */}
 
-            {/* Table */}
-            {/* Graph */}
+              
 
-            {/* Map */}
+              {/* Map */}
+              <Map />
+          </div>
+          <Card className="app__right">
+            <CardContent>
+              <h3>Live Cases by Country</h3>
+                {/* Table */}
+
+              <h3>Worldwide new cases</h3>
+                {/* Graph */}
+            </CardContent>
+          </Card> 
         </div>
     );
 }
