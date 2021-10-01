7e5eaad1560e0538d42a8e7133ba5729331cb228
patiltan4 <patiltan4@gmail.com>
2021-10-01 17:37:17 +0530

    added circles without data

--- src/App.js
+++ src/App.js
@@ -27,9 +27,10 @@ function App() {
     const [tableData, setTableData] = useState([]);
     const [mapCenter, setMapCenter] = useState({
         lat: 34.80746,
-        lng: -40.4796
+        lng: -40.4796,
     });
     const [mapZoom, setMapZoom] = useState(3);
+    const [mapCountries, setMapCountries] = useState([]);
 
     useEffect(() => {
         fetch("https://disease.sh/v3/covid-19/all")
@@ -52,6 +53,7 @@ function App() {
 
                     const sortedData = sortData(data);
                     setTableData(sortedData);
+                    setMapCountries(data);
                     setCountries(countries);
                 });
         };
@@ -78,6 +80,8 @@ function App() {
 
                 //All of the data from country response
                 setCountryInfo(data);
+                setMapCenter([data.countryInfo.lat, data.countryInfo.long]);
+                setMapZoom(4);
             });
     };
     console.log("country data", countryInfo);
@@ -133,7 +137,11 @@ function App() {
                 {/* info  */}
 
                 {/* Map */}
-                <Map center={mapCenter} zoom={mapZoom} />
+                <Map
+                    countries={mapCountries}
+                    center={mapCenter}
+                    zoom={mapZoom}
+                />
             </div>
             <Card className="app__right">
                 <CardContent>
--- src/Map.js
+++ src/Map.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Map as LeafletMap, TileLayer } from "react-leaflet";
 import "./Map.css";
+import { showDataOnMap } from "./util";
 
 //https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png
 //http://osm.org/copyright
-const Map = ({center, zoom}) => {
+const Map = ({ countries, casesType, center, zoom }) => {
     return (
         <div className="map">
             <LeafletMap center={center} zoom={zoom}>
@@ -12,6 +13,8 @@ const Map = ({center, zoom}) => {
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreet</a> contributors'
                 />
+                {/* map data */}
+                {showDataOnMap(countries, casesType)}
             </LeafletMap>
         </div>
     );
