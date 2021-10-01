34c51921cde7c483be0276b4088671a9ebea4ac3
patiltan4 <patiltan4@gmail.com>
2021-10-01 14:01:23 +0530

    added linegraph and chartjs2

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Map from "./Map";
 import Table from "./Table";
 import "./App.css";
 import { sortData } from "./util";
+import LineGraph from "./LineGraph";
 
 //https://disease.sh/v3/covid-19/countries
 
@@ -135,6 +136,7 @@ function App() {
                     <Table countries={tableData} />
 
                     <h3>Worldwide new cases</h3>
+                    <LineGraph />
                     {/* Graph */}
                 </CardContent>
             </Card>
